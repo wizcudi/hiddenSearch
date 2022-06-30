39532c0efa053215e6ae23abfbbad4434f7156e9
Tony Saint-Amour <tonysaint-amour@Spooky-Mac.local>
2022-06-30 15:22:58 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', ()=>{
+    search.classList.toggle('active') // toggle() adds a class; class added on search 
+    input.focus()
+})
+
+
+
+
+
+
+
+
+
+
